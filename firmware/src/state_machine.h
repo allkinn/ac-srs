effaa3e6ecec680a939f090d1fa067be4faeedc7
allkinn <ansarmubaroq@gmail.com>
2025-11-26 13:39:39 +0800

    Add state machine header for beam states

--- firmware/src/state_machine.h
+++ firmware/src/state_machine.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <Arduino.h>
 
+enum BeamState {
+    IDLE,
+    A_BROKEN,
+    B_BROKEN,
+    A_THEN_B,
+    B_THEN_A
+};
+
+extern BeamState beamState;
+
+void initStateMachine();
+void updateBeamState();
