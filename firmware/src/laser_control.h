9218470ef91f6504085d55f1060e720d3cf05ecc
allkinn <ansarmubaroq@gmail.com>
2025-11-27 09:22:36 +0800

    Add laser PWM initialization and night mode check

--- firmware/src/laser_control.h
+++ firmware/src/laser_control.h
@@ -1,5 +1,11 @@
 #pragma once
+#include <Arduino.h>
 
-void initLaserControl();
+// Inisialisasi pin laser & timing
+void initLaserPWM();
+
+// Update PWM sesuai day/night mode
 void updateLaserPWM();
+
+// Fungsi untuk menentukan apakah saat ini night mode
 bool isNightMode();
