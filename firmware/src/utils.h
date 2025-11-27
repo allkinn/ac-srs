56ec867f48f8670b8337c7a1af4494a245d551c3
allkinn <ansarmubaroq@gmail.com>
2025-11-27 10:29:30 +0800

    Add utility functions to utils.h
    Added utility functions for clamping, mapping with constraints, and debouncing.

--- firmware/src/utils.h
+++ firmware/src/utils.h
@@ -1,4 +1,11 @@
 #pragma once
+#include <Arduino.h>
 
-void logDataCSV();
-float getTemperature();
+// Clamp nilai antara min dan max
+int clamp(int value, int minVal, int maxVal);
+
+// Map dengan clamp otomatis
+int mapConstrain(int value, int inMin, int inMax, int outMin, int outMax);
+
+// Debounce simple untuk digital input (opsional, kalau butuh button)
+bool debounceRead(int pin, unsigned long debounceDelay = 50);
